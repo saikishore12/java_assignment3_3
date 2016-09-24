52529c47843638c0b14416b11426d5c934ac5dd8
Sai kishore Amirishetty <sai.kishi2010@gmail.com>
2016-09-24 21:29:54 +0530

    Assignment

---
+++ Assignment3_3/src/example.java
@@ -0,0 +1,10 @@
+
+public class example {
+
+	int x,y;
+	String name;
+	public static void main(String[] args) {
+	example pnt =new example();
+	System.out.println("pnt is " + pnt.name+""+ pnt.x+""+pnt.y);
+	}
+}
